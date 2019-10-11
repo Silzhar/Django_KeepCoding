525cea56dc044e24f25fb8fea9b6fbaef782419a
Silzhar <luis.ruizfer@gmail.com>
2019-10-11 20:11:52 +0200

    test runserver

--- photos/admin.py
+++ photos/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # from __future__ import unicode_literals
-from photos.models import Photo
+
 from django.contrib import admin
+from photos.models import Photo
 
 admin.site.register(Photo)
