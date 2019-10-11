fa72f51ded096721070699be529d4b370a8bb809
Silzhar <luis.ruizfer@gmail.com>
2019-10-11 20:12:10 +0200

    test runserver

---
+++ djangoProject/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/silzhar/Documentos/Python/django_KeepCoding/djangoProject/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
