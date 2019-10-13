2bb4fd8a7ab35a5815a38b53a0c50578e903f36a
Silzhar <luis.ruizfer@gmail.com>
2019-10-13 17:07:09 +0200

    add photos

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render
+from photos.models import Photo
 
 def home(request):
-    return HttpResponse("hola !")
+    photos = Photo.objects.all()
+    html = '<ul>'
+    for photo in photos:
+        html += '<li>' + photo.name + '</li>'
+    html += '</ul>'
+    return HttpResponse(html)
