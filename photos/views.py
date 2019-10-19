ee8df88d6cdfd34297147c48428f9086397f1ef9
Silzhar <luis.ruizfer@gmail.com>
2019-10-19 14:14:19 +0200

    limit 4 photos and descendent order

--- photos/views.py
+++ photos/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 from photos.models import Photo
 
 def home(request):
-    photos = Photo.objects.all()
+    photos = Photo.objects.all().order_by('-created_at') # query :for last photos created
     context = {
-        'photos_list': photos
+        'photos_list': photos[:4] # only 4 photos in home page
     }
     return render(request, 'photos/home.html', context)
 
