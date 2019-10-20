69f9b504f2faa022d2bd742f8124bb58c153dee9
Silzhar <luis.ruizfer@gmail.com>
2019-10-20 12:58:28 +0200

    visibility PUBLIC

--- photos/views.py
+++ photos/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render
-from photos.models import Photo
+from photos.models import Photo, PUBLIC
 
 def home(request):
-    photos = Photo.objects.all().order_by('-created_at') # query :for last photos created
+    photos = Photo.objects.filter(visibility=PUBLIC).order_by('-created_at') # query :for last photos created
     context = {
         'photos_list': photos[:4] # only 4 photos in home page
     }
