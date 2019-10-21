2ce8ede8588d3ede98400e68d839ac62023ac8f3
Silzhar <luis.ruizfer@gmail.com>
2019-10-21 21:59:08 +0200

    names urls

--- projectDjango/urls.py
+++ projectDjango/urls.py
@@ -20,6 +20,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^$', 'photos.views.home'), 
-    url(r'^photos/(?P<pk>[0-9]+)$', 'photos.views.detail') # (?P<pk>[0-9]+) = cualquier numero de 0 a 9 repetido una o mas veces
+    url(r'^$', 'photos.views.home', name='photos_home'), 
+    url(r'^photos/(?P<pk>[0-9]+)$', 'photos.views.detail', name='photo_detail') # (?P<pk>[0-9]+) = cualquier numero de 0 a 9 repetido una o mas veces
 ]
