50dc2fbc9c6f7494a2f8f18678f90d5bab868f9a
Silzhar <luis.ruizfer@gmail.com>
2019-11-01 12:56:28 +0100

    login() add LoginForm()

--- users/views.py
+++ users/views.py
@@ -4,20 +4,23 @@ from django.contrib.auth import logout as django_logout, authenticate, login as 
 from users.forms import LoginForm
 
 def login(request):
-    form = LoginForm()
     error_messages = []
     if request.method == 'POST':
-        username = request.POST.get('usr') # .get evita comflicto con contraseña errónea
-        password = request.POST.get('pwd')
-        user = authenticate(username=username, password=password) # busca encriptado de usuario y contraseña
-        if user is None:
-            error_messages.append('Nombre de usuario o contraseña incorrectos')
-        else:
-            if user.is_active:
-                django_login(request, user)
-                return ('photos_home')
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('usr') # .get evita comflicto con contraseña errónea
+            password = form.cleaned_data.get('pwd')
+            user = authenticate(username=username, password=password) # busca encriptado de usuario y contraseña
+            if user is None:
+                error_messages.append('Nombre de usuario o contraseña incorrectos')
             else:
-                error_messages.append('El usuario no está activo')
+                if user.is_active:
+                    django_login(request, user)
+                    return ('photos_home')
+                else:
+                    error_messages.append('El usuario no está activo')
+    else:
+        form = LoginForm()
                 
     context = {
         'errors': error_messages,
